3da68d2b8750fad39de5038d09d3855fa8181b19
studentAutomations <student.automations@gmail.com>
2024-11-23 15:00:29 +0100

    Update takephoto.py

--- takephoto.py
+++ takephoto.py
@@ -63,7 +63,7 @@ try:
     page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", responseT)
 
     # Take a screenshot of the entire element
-    responseT.screenshot('cs-lp-nova-obavestenja.png')
+    responseT.screenshot('cs-aor1-nova-obavestenja.png')
 
 finally:
     # Clean up by quitting the driver
