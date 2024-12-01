e075d5e7e6d50e6fa79f3e192d740cd1f59e4a2b
studentAutomations <student.automations@gmail.com>
2024-12-01 21:28:46 +0100

    Update discordBOT.py

--- discordBOT.py
+++ discordBOT.py
@@ -5,7 +5,7 @@ WEBHOOK_URL = [os.getenv('WEBHOOK_MAIN'), os.getenv('WEBHOOK_OTHER1')]
 for url in WEBHOOK_URL:
     hook = Webhook(url) 
 
-    hook.send('**@everyone CS je ažuriran!**')
+    hook.send('**@everyone AOR-1**')
     
     image2_path = 'cs-aor1-nova-obavestenja.png' 
 
