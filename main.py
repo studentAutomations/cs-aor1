832b854e3a93826b0404c1f067c7373d4e10c2a9
studentAutomations <student.automations@gmail.com>
2024-12-11 17:47:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 
 # Chrome options
 chrome_options = Options()
-chrome_options.add_argument("--headless")  
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.binary_location = "/usr/bin/google-chrome"
